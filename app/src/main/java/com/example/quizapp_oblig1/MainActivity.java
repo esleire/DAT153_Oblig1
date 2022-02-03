432dd838f96cc8dafc88a4472e6e979dea1f7e50
esleire <evensleire97@gmail.com>
2022-02-03 15:39:38 +0100

    add buttons on main activity

--- app/src/main/java/com/example/quizapp_oblig1/MainActivity.java
+++ app/src/main/java/com/example/quizapp_oblig1/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.quizapp_oblig1;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,42 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-}
+
+    public void clickAddButton(View view) {
+        Button button = (Button) findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_addstudent);
+
+
+            }
+        });
+
+    }
+    public void clickQuizButton(View view) {
+        Button button = (Button) findViewById(R.id.quizbutton);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_quiz);
+            }
+
+            });
+        }
+
+    public void clickDatabaseButton(View view) {
+        Button button = (Button) findViewById(R.id.quizbutton);
+        button.setOnClickListener(new View.OnClickListener(){
+
+            @Override
+            public void onClick(View view) {
+                setContentView(R.layout.activity_database);
+            }
+
+        });
+    }
+
+    }
