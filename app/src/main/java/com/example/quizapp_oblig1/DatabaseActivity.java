83555dea3c969c849b6fe8a840fe1f9a01124f59
esleire <evensleire97@gmail.com>
2022-02-06 12:06:26 +0100

    add sorting buttons and made updated view method

--- app/src/main/java/com/example/quizapp_oblig1/DatabaseActivity.java
+++ app/src/main/java/com/example/quizapp_oblig1/DatabaseActivity.java
@@ -1,11 +1,14 @@
 package com.example.quizapp_oblig1;
 
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.ArrayList;
@@ -21,12 +24,51 @@ public class DatabaseActivity extends AppCompatActivity {
      */
     private StudentList studentList = new StudentList();
 
+    @RequiresApi(api = Build.VERSION_CODES.N)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.listview);
-        List<Student> listOfStudents = studentList.getStudentList();
+        setUpdatedView();
 
+
+        /**
+         * Set listeners on sorting buttons
+         */
+
+        Button sortAlpha = findViewById(R.id.sortAlphabetically);
+        sortAlpha.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                studentList.sortedAlphabetically();
+                setUpdatedView();
+            }
+        });
+
+        Button sortReversed = findViewById(R.id.sortReversed);
+        sortReversed.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                studentList.sortReversedAlphabetically();
+                setUpdatedView();
+            }
+        });
+
+
+        Button newEntry = findViewById(R.id.addNewButton);
+        newEntry.setOnClickListener(view -> startActivity(new Intent(DatabaseActivity.this, AddStudentActivity.class)));
+
+
+
+    }
+
+    /**
+     * Method for displaying the entries in the database view.
+     * Will be called after sorting buttons is clicked
+     */
+
+    private void setUpdatedView(){
+        List<Student> listOfStudents = studentList.getStudentList();
         List<HashMap<String, String>> aList = new ArrayList<HashMap<String, String>>();
 
         for (Student s : listOfStudents) {
@@ -43,16 +85,6 @@ public class DatabaseActivity extends AppCompatActivity {
 
         ListView androidListView = findViewById(R.id.list_view);
         androidListView.setAdapter(simpleAdapter);
-
-        Button newEntry = findViewById(R.id.addNewButton);
-        newEntry.setOnClickListener(view -> startActivity(new Intent(DatabaseActivity.this, AddStudentActivity.class)));
-
-
-
-
-
-
-
     }
 
 }
